ba2f0ba92cbca9685f1e9709eceb84449b0f322d
rajranjan0608 <rranjan01234@gmail.com>
2022-05-23 15:22:25 +0530

    cf-connecting-ip

--- server.ts
+++ server.ts
@@ -68,7 +68,11 @@ app.get('/health', (req: any, res: any) => {
 });
 
 app.get('/ip', (req: any, res: any) => {
-    res.status(200).send(req.ip)
+    res.status(200).send({
+        reqip: req.ip,
+        xfor: req.headers["x-forwarded-for"],
+        cloudflare: req.headers["CF-Connecting-IP"]
+    })
 })
 
 app.get('*', async (req: any, res: any) => {
