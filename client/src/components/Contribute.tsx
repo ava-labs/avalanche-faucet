44716d5486ed82c8c83fcaf4ec45c758d8eb348e
rajranjan0608 <rranjan01234@gmail.com>
2022-05-24 00:49:55 +0530

    nits

--- client/src/components/Contribute.tsx
+++ client/src/components/Contribute.tsx
@@ -4,7 +4,7 @@ const Contribute = () => {
     return (
         <div className="contribute-button" onClick={() => {window.open('https://github.com/ava-labs/avalanche-faucet', '_blank')}}>
             <img style={{width: "25px", height: "25px", marginRight: "10px"}} src="/github.png"/>
-            Contribute to Github
+            Contribute on Github
         </div>
     )
 }
