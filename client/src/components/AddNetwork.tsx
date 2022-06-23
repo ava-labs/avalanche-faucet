7dcc745b4aa909112a3f8ce5d03b05c0921970af
rajranjan0608 <rranjan01234@gmail.com>
2022-06-23 13:21:09 +0530

    optimizations

--- client/src/components/AddNetwork.tsx
+++ client/src/components/AddNetwork.tsx
@@ -57,12 +57,12 @@ export default function AddNetwork(props: any) {
     return (
         <div className='footer-buttons'>
             <button className="add-network" onClick={() => {addNetwork(props.config)}}>
-                <img alt='metamask' style={{width: "25px", height: "25px", marginRight: "5px"}} src="/memtamask.png"/>
+                <img alt='metamask' style={{width: "25px", height: "25px", marginRight: "5px"}} src="/memtamask.webp"/>
                 Add Subnet to Metamask
             </button>
 
             <button className="add-network" onClick={() => {window.open(`${props.config.EXPLORER}`, '_blank')}}>
-                <img alt="block-explorer" style={{width: "25px", height: "25px"}} src="/avaxblack.png"/>
+                <img alt="block-explorer" style={{width: "25px", height: "25px"}} src="/avaxblack.webp"/>
                 View Block Explorer
             </button>
 
