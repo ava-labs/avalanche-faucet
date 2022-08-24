33348c99ca856097a85284e6b89822040ab85bf4
rajranjan0608 <rranjan01234@gmail.com>
2022-08-25 01:34:41 +0530

    typo

--- server.ts
+++ server.ts
@@ -98,7 +98,7 @@ erc20tokens.forEach((token: ERC20Type, i: number): void => {
 })
 
 // POST request for sending tokens or coins
-router.post('/sendToken', async (req: any, res: any) => {
+router.post('/sendToken', captcha.middleware, async (req: any, res: any) => {
     const address: string = req.body?.address
     const chain: string = req.body?.chain
     const erc20: string | undefined = req.body?.erc20
