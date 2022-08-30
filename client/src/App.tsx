54d3be520b798c24f9ac0adc0e218514c907c35d
rajranjan0608 <rranjan01234@gmail.com>
2022-08-30 17:36:09 +0530

    disclaimer and alerts

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,6 +7,11 @@ import './App.css'
 function App() {
   return (
     <div className="app">
+      <div style={{color: "#bdbdbd", border: "1px solid white", borderRadius: "4px", padding: "10px"}}>
+        The community updates the content on this page! We are <b>NOT</b> responsible
+        for it but we can remove them when brought to our notice. <a href='https://github.com/ava-labs/avalanche-faucet/blob/community-faucet/DISCLAIMER.md' target={"_blank"} rel="noreferrer">Learn more.</a>
+      </div>
+
       <FaucetForm axios = {axios} config = {config}/>
 
       <Contribute/>
