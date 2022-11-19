ca710cc2282cc6c9c699aa1f960a9eac9de105af
Josh Miller <mlxndrj@gmail.com>
2022-11-19 20:53:13 +0000

    fix: v2 recaptcha post request

--- middlewares/verifyCaptcha.ts
+++ middlewares/verifyCaptcha.ts
@@ -1,79 +1,98 @@
-const axios = require('axios')
+const axios = require("axios");
 
 export class VerifyCaptcha {
-    secret: string
-    v2secret?: string
-    middleware = (req: any, res: any, next: () => void) => this.verifyCaptcha(req, res, next)
+  secret: string;
+  v2secret: string;
+  middleware = (req: any, res: any, next: () => void) =>
+    this.verifyCaptcha(req, res, next);
 
-    constructor(app: any, CAPTCHA_SECRET: string, V2_CAPTCHA_SECRET?: string) {
-        if(typeof CAPTCHA_SECRET != "string") {
-            throw "Captcha Secret should be string"
-        }
-        this.secret = CAPTCHA_SECRET
-        this.v2secret = V2_CAPTCHA_SECRET
+  constructor(app: any, CAPTCHA_SECRET: string, V2_CAPTCHA_SECRET: string) {
+    if (typeof CAPTCHA_SECRET != "string") {
+      throw "Captcha Secret should be string";
+    }
+    this.secret = CAPTCHA_SECRET;
+    this.v2secret = V2_CAPTCHA_SECRET;
+  }
+
+  async verifyV2Token(v2Token: string): Promise<boolean> {
+    if (v2Token) {
+      const URL = "https://www.google.com/recaptcha/api/siteverify";
+      let response;
+
+      let recaptchaBody = {
+        secret: `${this.v2secret}`,
+        response: `${v2Token}`,
+      };
+
+      try {
+        response = await axios
+          .get(URL, {
+            headers: { "Content-Type": "application/x-www-form-urlencoded" },
+            params: recaptchaBody,
+          })
+          .then((r: any) => {
+            return r;
+          });
+      } catch (err: any) {
+        console.log("Recaptcha V2 error:", err?.message);
+      }
+
+      const data = response?.data;
+      console.log(JSON.stringify(data));
+
+      if (data?.success) {
+        return true;
+      }
     }
 
-    async verifyV2Token(v2Token: string): Promise<boolean> {
-        if(v2Token) {
-            const URL = `https://www.google.com/recaptcha/api/siteverify?secret=${this.v2secret}&response=${v2Token}`
-            let response
+    return false;
+  }
 
-            try {
-                response = await axios.post(URL)
-            } catch(err: any){
-                console.log("Recaptcha V2 error:", err?.message)
-            }
+  async verifyV3Token(v3Token: string): Promise<boolean> {
+    const URL = `https://www.google.com/recaptcha/api/siteverify?secret=${this.secret}&response=${v3Token}`;
+    let response;
 
-            const data = response?.data
-
-            if(data?.success) {
-                return true;
-            }
-        }
-
-        return false
+    try {
+      response = await axios.post(URL);
+    } catch (err: any) {
+      console.log("Recaptcha V3 error:", err?.message);
     }
 
-    async verifyV3Token(v3Token: string): Promise<boolean> {
-        const URL = `https://www.google.com/recaptcha/api/siteverify?secret=${this.secret}&response=${v3Token}`
-        let response
+    const data = response?.data;
 
-        try {
-            response = await axios.post(URL)
-        } catch(err: any){
-            console.log("Recaptcha V3 error:", err?.message)
+    if (data?.success) {
+      if (data?.action == "faucetdrip") {
+        if (data?.score > 0.5) {
+          return true;
         }
-        
-        const data = response?.data
-
-        if(data?.success) {
-            if(data?.action == 'faucetdrip') {
-                if(data?.score > 0.5) {
-                    return true
-                }
-            }
-        }
-
-        return false
+      }
     }
 
-    async shouldAllow(token: string, v2Token: string): Promise<boolean> {
-        if(await this.verifyV3Token(token)) {
-            return true
-        } else {
-            if(await this.verifyV2Token(v2Token)) {
-                return true
-            }
-        }
-        return false
-    }
+    return false;
+  }
 
-    async verifyCaptcha(req: any, res: any, next: () => void) {
-        const shouldAllow = await this.shouldAllow(req?.body?.token, req?.body?.v2Token)
-        if(shouldAllow) {
-            next()
-        } else {
-            return res.status(400).send({message: "Captcha verification failed! Try solving below."})
-        }
+  async shouldAllow(token: string, v2Token: string): Promise<boolean> {
+    if (await this.verifyV3Token(token)) {
+      return true;
+    } else {
+      if (await this.verifyV2Token(v2Token)) {
+        return true;
+      }
     }
+    return false;
+  }
+
+  async verifyCaptcha(req: any, res: any, next: () => void) {
+    const shouldAllow = await this.shouldAllow(
+      req?.body?.token,
+      req?.body?.v2Token
+    );
+    if (shouldAllow) {
+      next();
+    } else {
+      return res
+        .status(400)
+        .send({ message: "Captcha verification failed! Try solving below." });
+    }
+  }
 }
