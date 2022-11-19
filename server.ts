4a521d8602049e897687cec06d2dbe69ae73a16a
Josh Miller <mlxndrj@gmail.com>
2022-11-19 20:53:55 +0000

    fix: v2 secret

--- server.ts
+++ server.ts
@@ -1,172 +1,174 @@
-import express from 'express'
-import bodyParser from 'body-parser'
-import cors from 'cors'
-import path from 'path'
-import dotenv from 'dotenv'
-import { BN } from 'avalanche'
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import path from "path";
+import dotenv from "dotenv";
+import { BN } from "avalanche";
 
-import { RateLimiter, VerifyCaptcha, parseURI } from './middlewares'
-import EVM from './vms/evm'
-
-import {
-    SendTokenResponse,
-    ChainType,
-    EVMInstanceAndConfig,
-    ERC20Type
-} from './types'
+import { RateLimiter, VerifyCaptcha, parseURI } from "./middlewares";
+import EVM from "./vms/evm";
 
 import {
-    evmchains,
-    erc20tokens,
-    GLOBAL_RL
-} from './config.json'
+  SendTokenResponse,
+  ChainType,
+  EVMInstanceAndConfig,
+  ERC20Type,
+} from "./types";
 
-dotenv.config()
+import { evmchains, erc20tokens, GLOBAL_RL } from "./config.json";
 
-const app: any = express()
-const router: any = express.Router()
+dotenv.config();
 
-app.use(express.static(path.join(__dirname, "client")))
-app.use(cors())
-app.use(parseURI)
-app.use(bodyParser.json())
+const app: any = express();
+const router: any = express.Router();
 
-new RateLimiter(app, [GLOBAL_RL])
+app.use(express.static(path.join(__dirname, "client")));
+app.use(cors());
+app.use(parseURI);
+app.use(bodyParser.json());
 
-new RateLimiter(app, [
-    ...evmchains,
-    ...erc20tokens
-])
+new RateLimiter(app, [GLOBAL_RL]);
+
+new RateLimiter(app, [...evmchains, ...erc20tokens]);
 
 // address rate limiter
-new RateLimiter(app, [
-    ...evmchains,
-    ...erc20tokens
-], (req: any, res: any) => {
-    const addr = req.body?.address
+new RateLimiter(app, [...evmchains, ...erc20tokens], (req: any, res: any) => {
+  const addr = req.body?.address;
 
-    if(typeof addr == "string" && addr) {
-        return addr.toUpperCase()
-    }
-})
+  if (typeof addr == "string" && addr) {
+    return addr.toUpperCase();
+  }
+});
 
-const captcha: VerifyCaptcha = new VerifyCaptcha(app, process.env.CAPTCHA_SECRET!, process.env.V2_CAPTCHA_SECRET)
+const captcha: VerifyCaptcha = new VerifyCaptcha(
+  app,
+  process.env.CAPTCHA_SECRET!,
+  process.env.V2_CAPTCHA_SECRET!
+);
 
-let evms = new Map<string, EVMInstanceAndConfig>()
+let evms = new Map<string, EVMInstanceAndConfig>();
 
 // Get the complete config object from the array of config objects (chains) with ID as id
-const getChainByID = (chains: ChainType[], id: string): ChainType | undefined => {
-    let reply: ChainType | undefined
-    chains.forEach((chain: ChainType): void => {
-        if(chain.ID == id) {
-            reply = chain
-        }
-    })
-    return reply
-}
+const getChainByID = (
+  chains: ChainType[],
+  id: string
+): ChainType | undefined => {
+  let reply: ChainType | undefined;
+  chains.forEach((chain: ChainType): void => {
+    if (chain.ID == id) {
+      reply = chain;
+    }
+  });
+  return reply;
+};
 
 // Populates the missing config keys of the child using the parent's config
 const populateConfig = (child: any, parent: any): any => {
-    Object.keys(parent || {}).forEach((key) => {
-        if(!child[key]) {
-            child[key] = parent[key]
-        }
-    })
-    return child
-}
+  Object.keys(parent || {}).forEach((key) => {
+    if (!child[key]) {
+      child[key] = parent[key];
+    }
+  });
+  return child;
+};
 
 // Setting up instance for EVM chains
 evmchains.forEach((chain: ChainType): void => {
-    const chainInstance: EVM = new EVM(chain, process.env[chain.ID] || process.env.PK)
-    
-    evms.set(chain.ID, {
-        config: chain,
-        instance: chainInstance
-    })
-})
+  const chainInstance: EVM = new EVM(
+    chain,
+    process.env[chain.ID] || process.env.PK
+  );
+
+  evms.set(chain.ID, {
+    config: chain,
+    instance: chainInstance,
+  });
+});
 
 // Adding ERC20 token contracts to their HOST evm instances
 erc20tokens.forEach((token: ERC20Type, i: number): void => {
-    if(token.HOSTID) {
-        token = populateConfig(token, getChainByID(evmchains, token.HOSTID))
-    }
+  if (token.HOSTID) {
+    token = populateConfig(token, getChainByID(evmchains, token.HOSTID));
+  }
 
-    erc20tokens[i] = token
-    const evm: EVMInstanceAndConfig = evms.get(getChainByID(evmchains, token.HOSTID)?.ID!)!
+  erc20tokens[i] = token;
+  const evm: EVMInstanceAndConfig = evms.get(
+    getChainByID(evmchains, token.HOSTID)?.ID!
+  )!;
 
-    evm?.instance.addERC20Contract(token)
-})
+  evm?.instance.addERC20Contract(token);
+});
 
 // POST request for sending tokens or coins
-router.post('/sendToken', captcha.middleware, async (req: any, res: any) => {
-    const address: string = req.body?.address
-    const chain: string = req.body?.chain
-    const erc20: string | undefined = req.body?.erc20
+router.post("/sendToken", captcha.middleware, async (req: any, res: any) => {
+  const address: string = req.body?.address;
+  const chain: string = req.body?.chain;
+  const erc20: string | undefined = req.body?.erc20;
 
-    const evm: EVMInstanceAndConfig = evms.get(chain)!
+  const evm: EVMInstanceAndConfig = evms.get(chain)!;
 
-    if(evm) {
-        evm?.instance.sendToken(address, erc20, (data: SendTokenResponse) => {
-            const { status, message, txHash } = data
-            res.status(status).send({message, txHash})
-        })
-    } else {
-        res.status(400).send({message: "Invalid parameters passed!"})
-    }
-})
+  if (evm) {
+    evm?.instance.sendToken(address, erc20, (data: SendTokenResponse) => {
+      const { status, message, txHash } = data;
+      res.status(status).send({ message, txHash });
+    });
+  } else {
+    res.status(400).send({ message: "Invalid parameters passed!" });
+  }
+});
 
 // GET request for fetching all the chain and token configurations
-router.get('/getChainConfigs', (req: any, res: any) => {
-    const configs: any = [...evmchains, ...erc20tokens]
-    res.send({ configs })
-})
+router.get("/getChainConfigs", (req: any, res: any) => {
+  const configs: any = [...evmchains, ...erc20tokens];
+  res.send({ configs });
+});
 
 // GET request for fetching faucet address for the specified chain
-router.get('/faucetAddress', (req: any, res: any) => {
-    const chain: string = req.query?.chain
-    const evm: EVMInstanceAndConfig = evms.get(chain)!
+router.get("/faucetAddress", (req: any, res: any) => {
+  const chain: string = req.query?.chain;
+  const evm: EVMInstanceAndConfig = evms.get(chain)!;
 
-    res.send({
-        address: evm?.instance.account.address
-    })
-})
+  res.send({
+    address: evm?.instance.account.address,
+  });
+});
 
 // GET request for fetching faucet balance for the specified chain or token
-router.get('/getBalance', (req: any, res: any) => {
-    const chain: string = req.query?.chain
-    const erc20: string | undefined = req.query?.erc20
+router.get("/getBalance", (req: any, res: any) => {
+  const chain: string = req.query?.chain;
+  const erc20: string | undefined = req.query?.erc20;
 
-    const evm: EVMInstanceAndConfig = evms.get(chain)!
+  const evm: EVMInstanceAndConfig = evms.get(chain)!;
 
-    let balance: BN = evm?.instance.getBalance(erc20)
+  let balance: BN = evm?.instance.getBalance(erc20);
 
-    if(balance) {
-        balance = balance
-    } else {
-        balance = new BN(0)
-    }
+  if (balance) {
+    balance = balance;
+  } else {
+    balance = new BN(0);
+  }
 
-    res.status(200).send({
-        balance: balance?.toString()
-    })
-})
+  res.status(200).send({
+    balance: balance?.toString(),
+  });
+});
 
-app.use('/api', router)
+app.use("/api", router);
 
-app.get('/health', (req: any, res: any) => {
-    res.status(200).send('Server healthy')
-})
+app.get("/health", (req: any, res: any) => {
+  res.status(200).send("Server healthy");
+});
 
-app.get('/ip', (req: any, res: any) => {
-    res.status(200).send({
-        ip: req.headers["cf-connecting-ip"] || req.ip
-    })
-})
+app.get("/ip", (req: any, res: any) => {
+  res.status(200).send({
+    ip: req.headers["cf-connecting-ip"] || req.ip,
+  });
+});
 
-app.get('*', async (req: any, res: any) => {
-    res.sendFile(path.join(__dirname, "client", "index.html"))
-})
+app.get("*", async (req: any, res: any) => {
+  res.sendFile(path.join(__dirname, "client", "index.html"));
+});
 
 app.listen(process.env.PORT || 8000, () => {
-    console.log(`Server started at port ${process.env.PORT || 8000}`)
-})
+  console.log(`Server started at port ${process.env.PORT || 8000}`);
+});
