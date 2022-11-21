20d14a3b6301407ffbce1a61d69eea3c2de01a48
rajranjan0608 <rranjan01234@gmail.com>
2022-11-21 18:41:39 +0530

    fixed widget render for v2 captcha

--- client/src/components/ReCaptcha.ts
+++ client/src/components/ReCaptcha.ts
@@ -8,20 +8,18 @@ export default class ReCaptcha {
     siteKey: string
     v2siteKey?: string
     action: string
-    widgetID: string | undefined
     setWidgetID: any
         
-    constructor(SITE_KEY: string, ACTION: string, V2_SITE_KEY: string, setWidgetID: any, widgetID: string | undefined) {
+    constructor(SITE_KEY: string, ACTION: string, V2_SITE_KEY: string, setWidgetID: any) {
         this.loadReCaptcha(SITE_KEY)
         
         this.siteKey = SITE_KEY
         this.v2siteKey = V2_SITE_KEY
         this.action = ACTION
-        this.widgetID = widgetID
         this.setWidgetID = setWidgetID
     }
 
-    async getToken(isV2 = false): Promise<{token?: string, v2Token?: string}> {
+    async getToken(isV2 = false, widgetID: any, index: number = 0): Promise<{token?: string, v2Token?: string}> {
         let token = "", v2Token = ""
         !isV2 && await window.grecaptcha.execute(this.siteKey, {action: this.action})
             .then((res: string) => {
@@ -29,35 +27,37 @@ export default class ReCaptcha {
             })
         
         if(isV2){
-            v2Token = await window.grecaptcha.getResponse(this.widgetID)
+            v2Token = await window.grecaptcha.getResponse(widgetID.get(index))
         }
 
         return { token, v2Token }
     }
 
-    resetV2Captcha = () => {
-        const v2CaptchaContainer = <HTMLElement>document.getElementsByClassName('v2-recaptcha')[0]
+    resetV2Captcha = (widgetID: any, index: number = 0) => {
+        const v2CaptchaContainer = document.getElementsByClassName('v2-recaptcha')[index] as HTMLElement
         if(v2CaptchaContainer) {
-            if(this.widgetID) {
-                window.grecaptcha.reset(this.widgetID)
+            if(widgetID.get(index) || widgetID.get(index) == 0) {
+                window.grecaptcha.reset(widgetID.get(index))
             }
             v2CaptchaContainer.style.display = "none"
         }
     }
 
-    loadV2Captcha = (v2siteKey: string) => {
-        const v2CaptchaContainer = document.getElementsByClassName('v2-recaptcha')[0]
-
-        if(this.widgetID || this.widgetID == "0") {
-            const v2CaptchaContainer = <HTMLElement>document.getElementsByClassName('v2-recaptcha')[0]
+    loadV2Captcha = (v2siteKey: string, widgetID: any, index: number = 0, reload: boolean = false) => {
+        this.resetV2Captcha(widgetID, index)
+        const v2CaptchaContainer = document.getElementsByClassName('v2-recaptcha')[index] as HTMLElement
+        if((widgetID.get(index) || widgetID.get(index) == "0") && !reload) {
+            const v2CaptchaContainer = document.getElementsByClassName('v2-recaptcha')[index] as HTMLElement
             if(v2CaptchaContainer) {
                 v2CaptchaContainer.style.display = "block"
             }
         } else {
-            const widgetID = window.grecaptcha.render(v2CaptchaContainer, {
+            v2CaptchaContainer.style.display = "block"
+            const newWidgetID = window.grecaptcha.render(v2CaptchaContainer, {
                 'sitekey' : v2siteKey,
                 'theme': 'dark'
             })
+            widgetID.set(index, newWidgetID)
             this.setWidgetID(widgetID)
         }
 
