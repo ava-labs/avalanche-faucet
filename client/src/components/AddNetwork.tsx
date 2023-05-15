3a223ac408596c9149594e85e7c0aaee86533a6e
serejke <serejke.best@gmail.com>
2023-05-15 18:40:04 +0100

    chore: Khalani branding.

--- client/src/components/AddNetwork.tsx
+++ client/src/components/AddNetwork.tsx
@@ -9,7 +9,7 @@ export default function AddNetwork(props: any) {
             </button>
 
             <button className="add-network" onClick={() => {window.open(`${props.config.EXPLORER}${props.token?.CONTRACTADDRESS ? "/address/" + props.token.CONTRACTADDRESS : ""}`, '_blank')}}>
-                <img alt="block-explorer" style={{width: "25px", height: "25px"}} src="/avaxblack.webp"/>
+                <img alt="block-explorer" style={{width: "25px", height: "25px"}} src="/khalani-logo.png"/>
                 View Block Explorer
             </button>
 
