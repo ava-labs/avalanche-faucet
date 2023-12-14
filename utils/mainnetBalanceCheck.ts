95793f92f956dff9f762c906160a34e84247b321
Raj Ranjan <rranjan01234@gmail.com>
2023-12-15 02:39:19 +0530

    Development (mainnet balance check) (#128)
    * mainnet balance checks
    
    * cfg
    
    * logs
    
    * typo
    
    * json
    
    * nits

---
+++ utils/mainnetBalanceCheck.ts
@@ -0,0 +1,26 @@
+import axios from "axios"
+
+export async function checkMainnetBalance(faucetConfigId: string, rpc: string, address: string, threshold = 0): Promise<boolean> {
+    try {
+        const response = await axios.post<any, any>(rpc, {
+            jsonrpc: "2.0",
+            method: "eth_getBalance",
+            params: [address, "latest"],
+            id: 1,
+        })
+        const balance = parseInt(response.data.result)
+        if (balance > threshold) {
+            console.log("Successful FaucetMainnetBalanceCheck:", JSON.stringify({
+                type: "FaucetMainnetBalanceCheckSuccess",
+                chain: faucetConfigId,
+                address: address,
+                balance: balance,
+            }))
+            return true
+        }
+    } catch(err) {
+        console.error('ERROR: checkMainnetBalance', err)
+        return false
+    }
+    return false
+}
